7ee5e177f72163d8c861c327a618c34342cb551e
Garfield247 <Garfield_lv@163.com>
2019-01-14 14:08:18 +0800

    start project and build project tree

---
+++ project/app/config.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Date    : 2019-01-14 14:03:24
+# @Author  : LvGang/Garfield
+# @Email   : Garfield_lv@163.com
+
+
+import os
+
---
+++ project/app/email.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Date    : 2019-01-14 14:04:15
+# @Author  : LvGang/Garfield
+# @Email   : Garfield_lv@163.com
+
+
+import os
+
